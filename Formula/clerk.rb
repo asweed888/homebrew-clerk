e3642cec59d5b9f92baed86d11f9db467c0036d0
inadati <dr.inadati@gmail.com>
2022-09-09 16:57:37 +0900

    Brew formula update for clerk version v0.1.1

--- Formula/clerk.rb
+++ Formula/clerk.rb
@@ -5,11 +5,11 @@
 class Clerk < Formula
   desc "Declarative development framework clerk"
   homepage "https://github.com/asweed888/clerk"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
-    url "https://github.com/asweed888/clerk/releases/download/v0.1.0/clerk_darwin_all.tar.gz"
-    sha256 "bba34cff7ab393979f26b6e89072e2727eb734c01b5075eaa26c5fe5b7182a63"
+    url "https://github.com/asweed888/clerk/releases/download/v0.1.1/clerk_darwin_all.tar.gz"
+    sha256 "aeacebf7bb8d723a972ce5d04feacf56f6c5c397310d375b1015b5150b6b2d13"
 
     def install
       bin.install "clerk"
@@ -18,24 +18,24 @@ class Clerk < Formula
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/asweed888/clerk/releases/download/v0.1.0/clerk_linux_armv6.tar.gz"
-      sha256 "74a9d78621532833120e59b11bbcc8d258041f07b30a0e30434ccfbf32ea177d"
+      url "https://github.com/asweed888/clerk/releases/download/v0.1.1/clerk_linux_armv6.tar.gz"
+      sha256 "57d8bdd2f2c477665cab8715a470950d8f620a0a2c63ae7ff9b969ab2532bcde"
 
       def install
         bin.install "clerk"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/asweed888/clerk/releases/download/v0.1.0/clerk_linux_arm64.tar.gz"
-      sha256 "035c3b67ada6d720520febd67aef3f2d8d2265947ca0deb7fb9cd5e06f95f582"
+      url "https://github.com/asweed888/clerk/releases/download/v0.1.1/clerk_linux_arm64.tar.gz"
+      sha256 "fa3079e434a6d7727676eb4449b19d9a963e9abf2b5f79b482b86b2e4a607aa3"
 
       def install
         bin.install "clerk"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/asweed888/clerk/releases/download/v0.1.0/clerk_linux_x86_64.tar.gz"
-      sha256 "2dbf06fbdd0388eadd2732ec2fb116fbeaea210b08529008f0c8f25b45051165"
+      url "https://github.com/asweed888/clerk/releases/download/v0.1.1/clerk_linux_x86_64.tar.gz"
+      sha256 "85cd90fdba3ca015488ef00f1201e3cc37fcf9d6056d0d606d5ab1eb7c7f374c"
 
       def install
         bin.install "clerk"
