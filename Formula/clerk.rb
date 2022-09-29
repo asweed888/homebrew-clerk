29790bedb0a4268cfad8ab8e5c518de47d3fbe4e
inadati <dr.inadati@gmail.com>
2022-09-29 13:35:29 +0900

    Brew formula update for clerk version v0.2.2

--- Formula/clerk.rb
+++ Formula/clerk.rb
@@ -5,11 +5,11 @@
 class Clerk < Formula
   desc "Declarative development framework clerk"
   homepage "https://github.com/asweed888/clerk"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
-    url "https://github.com/asweed888/clerk/releases/download/v0.2.1/clerk_darwin_all.tar.gz"
-    sha256 "225f29bb61f7bc51bfad02d5f6588481ad47b21c65b7ed3941138f90700ef598"
+    url "https://github.com/asweed888/clerk/releases/download/v0.2.2/clerk_darwin_all.tar.gz"
+    sha256 "e21193ac3a9a18457c29d962093e95c6b21e40fb468f66a7e11b06d8b4922f8a"
 
     def install
       bin.install "clerk"
@@ -17,25 +17,25 @@ class Clerk < Formula
   end
 
   on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/asweed888/clerk/releases/download/v0.2.2/clerk_linux_arm64.tar.gz"
+      sha256 "762df8a7ed09b1e443e8a800eb1313c41900b59aa9b03c7a92b4523e4e887f0f"
+
+      def install
+        bin.install "clerk"
+      end
+    end
     if Hardware::CPU.intel?
-      url "https://github.com/asweed888/clerk/releases/download/v0.2.1/clerk_linux_x86_64.tar.gz"
-      sha256 "941697e622cbaa373b4f08c25e97b8f8131e4040eb3a8e390567010b64412a34"
+      url "https://github.com/asweed888/clerk/releases/download/v0.2.2/clerk_linux_x86_64.tar.gz"
+      sha256 "9d950a639e24075bf64aa7275f38235805b73502262db01539535fed8e78eaa6"
 
       def install
         bin.install "clerk"
       end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/asweed888/clerk/releases/download/v0.2.1/clerk_linux_armv6.tar.gz"
-      sha256 "24110f16b2c62dfffab469a193d222cf6c3cb4ba7700fd2f412820546c52d4ff"
-
-      def install
-        bin.install "clerk"
-      end
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/asweed888/clerk/releases/download/v0.2.1/clerk_linux_arm64.tar.gz"
-      sha256 "c880bfd952d432feef4ab7eb386039d39e3f661045c270bb1731ba94521ddc9e"
+      url "https://github.com/asweed888/clerk/releases/download/v0.2.2/clerk_linux_armv6.tar.gz"
+      sha256 "88845824c670b6b644484507c805e08551820544248432e32198d4345d2e0396"
 
       def install
         bin.install "clerk"
